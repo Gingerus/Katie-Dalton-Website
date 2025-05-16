3c25ffcb1037f681e3a4c0be6b1085b97902082c
W0420453 <W0420453@campus.nscc.ca>
2025-05-16 16:10:52 -0300

    Slideshow Fix, Our Story First Draft

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -23,6 +23,11 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  console.log(slides);
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+window.onload = function() { showSlides(1); };
+
+
